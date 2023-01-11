2436ff9908ec769dfa1d384e9a74f1451955f009
Zion Miller <djzionmiller@gmail.com>
2023-01-10 16:19:42 -0800

    frontend assesment with prelim styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { IPost } from "./types";
 
 function App() {
+  
+  const [post, setPost] = useState<IPost[]>([])
+
+  useEffect(() => {
+    fetch("/api/posts")
+    .then((res) => res.json())
+    .then(data => setPost(data))
+  },[])
+
   return (
     <div className="App">
-      <h1>Hello, Dubsado!</h1>
+      <div>Hello Dubsado</div>
+      {
+        post.map((pos) =>
+        <div id="box">
+          <h2>{pos.handle}</h2>
+          <img id='avatar' src={pos.avatar}/>
+          <img src={pos.image} />
+          <h3>{pos.likes}</h3>
+          <h3>{pos.caption}</h3>
+        </div>
+        )
+      }
     </div>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,12 @@ export interface IPost {
   likes: number;
   caption: string;
 }
+
+export const dummyPost: IPost = {
+  id: 1,
+  handle: "dummy1",
+  avatar: "dumm1Avatar",
+  image: "dummyImg",
+  likes: 2,
+  caption: "dummyCap",
+}
