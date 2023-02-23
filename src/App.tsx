f28cbc80262b0f4b752c2a4d71481d22ea179f05
Justin Bird <justin.bird@dubsado.com>
2023-02-23 08:01:40 -0800

    base counter ui. non-functional.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello, Dubsado!</h1>
+      <h1>Counter</h1>
+      <div className="counter">
+        <div className="counter__display">0</div>
+        <div className="counter__buttons">
+          <button className="counter__button">-</button>
+          <button className="counter__button">+</button>
+        </div>
+      </div>
     </div>
   );
 }
